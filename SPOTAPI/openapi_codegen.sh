d4b10042d134dadff151d1c2d05e4084d809f618
Your Name <you@example.com>
2021-02-09 21:25:32 +0100

    sd

---
+++ SPOTAPI/openapi_codegen.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#set -ev
+openapi_file="openapi-spot.yaml"
+out_framework="python-flask"
+package_name="SPOTAPI"
+
+openapi_file=${1:-$openapi_file}
+out_framework=${2:-$out_framework}
+package_name=${3:-$package_name}
+
+docker run --rm \
+      -v ${PWD}:/local openapitools/openapi-generator-cli generate \
+      -i /local/$openapi_file \
+      -g $out_framework \
+      --package-name $package_name \
+      -o /local/$package_name
+
+# Change right of output folder
+sudo chown -R $USER:$USER ./$package_name
--- data/scripts/2.InsertDatas.sql
+++ data/scripts/2.InsertDatas.sql
@@ -1,61 +1,61 @@
 LOAD DATA INFILE "/var/lib/mysql-files/ingredient_in_recipe.csv"
-INTO TABLE `ingredient_in_recipe`
+INTO TABLE Ingredient_In_Recipe
 COLUMNS TERMINATED BY ';'
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
-LOAD DATA INFILE "/var/lib/mysql-files/ingredient_Type.csv"
-INTO TABLE `Ingredient_Type`
+LOAD DATA INFILE "/var/lib/mysql-files/ingredient_type.csv"
+INTO TABLE Ingredient_Type
 COLUMNS TERMINATED BY ';'
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
 LOAD DATA INFILE "/var/lib/mysql-files/ingredient.csv"
-INTO TABLE `ingredient`
+INTO TABLE Ingredient
 COLUMNS TERMINATED BY ';'
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
 LOAD DATA INFILE "/var/lib/mysql-files/recipes.csv"
-INTO TABLE `Recipes`
+INTO TABLE Recipes
 COLUMNS TERMINATED BY ';'
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
 LOAD DATA INFILE "/var/lib/mysql-files/regime_alimentaire.csv"
-INTO TABLE `regime_alimentaire`
+INTO TABLE Regime_Alimentaire
 COLUMNS TERMINATED BY ';'
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
 LOAD DATA INFILE "/var/lib/mysql-files/regime_recette.csv"
-INTO TABLE `regime_recette`
+INTO TABLE regime_recette
 COLUMNS TERMINATED BY ';'
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
 LOAD DATA INFILE "/var/lib/mysql-files/regime_users.csv"
-INTO TABLE `regime_users`
+INTO TABLE regime_users
 COLUMNS TERMINATED BY ';'
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
 LOAD DATA INFILE "/var/lib/mysql-files/relation_ingredient_user.csv"
-INTO TABLE `relation_ingredient_user`
+INTO TABLE relation_Ingredient_User
 COLUMNS TERMINATED BY ';'
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
 LOAD DATA INFILE "/var/lib/mysql-files/users.csv"
-INTO TABLE `users`
+INTO TABLE Users
 COLUMNS TERMINATED BY ';'
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
